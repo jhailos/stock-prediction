3aa1fd6e5b928d1790007d6df941b696e446f350
Louan Flammanc <45247162+Gloobinours@users.noreply.github.com>
2024-10-08 20:54:21 -0400

    features computing functionality

--- sklearn/bagging_lsvm.py
+++ sklearn/bagging_lsvm.py
@@ -9,10 +9,23 @@ def download_data(ticker, interval='15m'):
     """Fetch data from yfinance
     """
     end_date = datetime.datetime.now()
-    start_date = end_date - datetime.timedelta(days=60)
+    start_date = end_date - datetime.timedelta(days=59)
 
     data = yf.download(ticker, start=start_date, end=end_date, interval=interval)
 
+    return data
+
+def compute_features(data):
+    # EMA
+    data['EMA12'] = data['Close'].ewm(span=12).mean()
+    data['EMA26'] = data['Close'].ewm(span=26).mean()
+
+    # MACD
+    data['MACD'] = data['EMA12'] + data['EMA26']
+
+    #MACD signal
+    data['MACD_signal'] = data['MACD'].ewm(span=9).mean()
+    
     return data
 
 def main():
@@ -21,6 +34,8 @@ def main():
     
     # Download data
     data = download_data(ticker)
+
+    print(compute_data(data))
 
 if __name__ == "__main__":
     main()
