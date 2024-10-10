d397b504aee83a92d060b3880eb232c0a3176c33
Louan Flammanc <45247162+Gloobinours@users.noreply.github.com>
2024-10-10 14:51:09 -0400

    added next closing date price prediction [has warning]

--- sklearn/bagging_lsvm.py
+++ sklearn/bagging_lsvm.py
@@ -5,21 +5,21 @@ import datetime
 
 import sklearn
 from sklearn.svm import LinearSVR
-from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import BaggingRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 """
 
 """
-def download_data(ticker, interval='15m'):
+def download_data(ticker, interval='5m'):
     """Fetch data from yfinance
     """
     end_date = datetime.datetime.now()
     start_date = end_date - datetime.timedelta(days=59)
 
     data = yf.download(ticker, start=start_date, end=end_date, interval=interval)
-
+    data.dropna(inplace=True)
     return data
 
 def compute_features(data):
@@ -39,27 +39,26 @@ def compute_features(data):
     # Previous closing price
     data['previous_close'] = data['Close'].shift(1)
     
+    data.dropna(inplace=True)
+
     return data
 
-def train_model(x, y):
-    linearSVR = LinearSVR(max_iter=100) #! Unsure max_iter
-    bagging_model = BaggingClassifier(estimator=linearSVR, n_estimators=100)
-    bagging_model.fit(x, y)
+def train_model(x_train, y_train):
+    linear_SVR = LinearSVR(max_iter=100000) #! Unsure max_iter
+    bagging_model = BaggingRegressor(estimator=linear_SVR, n_estimators=100)
+    bagging_model.fit(x_train, y_train)
 
     return bagging_model
 
 def data_preprocessing(data):
     y = data['Close'].shift(-1) # Y is value to predict (price in the next interval)
     data['y'] = y
-    #! Added drop NA but paper says to : "use the method of imputing with the prior existing values to handle missing values"
+    data.dropna(inplace=True) #! Added drop NA but paper says to : "use the method of imputing with the prior existing values to handle missing values"
     x = data[['EMA12', 'EMA26', 'MACD', 'MACD_signal', 'price_change', 'previous_close']]
 
-    data.dropna(inplace=True)
+    return x, data['y']
 
-    return x, y
-
-def scale_data(x_train, x_test):
-    scaler = StandardScaler()
+def scale_data(x_train, x_test, scaler):
     x_train_scaled = scaler.fit_transform(x_train)
     x_test_scaled = scaler.transform(x_test)
 
@@ -71,6 +70,14 @@ def model_eval(model, x_test, y_test):
     r_squared = r2_score(y_test, prediction)
 
     return rmse, r_squared
+
+def next_closing(data, model, scaler):
+    most_recent = data.iloc[-1][['EMA12', 'EMA26', 'MACD', 'MACD_signal', 'price_change', 'previous_close']].values.reshape(1, -1)
+    scaled = scaler.transform(most_recent)
+    
+    prediction = model.predict(scaled)
+
+    return prediction[0]
 
 def main():
     # Main function to run the analysis
@@ -85,14 +92,19 @@ def main():
     # Pre proc
     x, y = data_preprocessing(data)
 
+
+    # Scaler
+    scaler = StandardScaler()
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-    x_train_scaled, x_train_scaled = scale_data(x_train, x_test)
+    x_train_scaled, x_test_scaled = scale_data(x_train, x_test, scaler)
 
     model = train_model(x_train_scaled, y_train)
 
-    model_eval(model, x_train_scaled, y_test)
-
+    rmse, r2 = model_eval(model, x_test_scaled, y_test)
+    print("RMSE: ", rmse)
+    print("R2: ", r2)
+    print("Price in next interval: ", next_closing(data, model, scaler))
 
 if __name__ == "__main__":
     main()
