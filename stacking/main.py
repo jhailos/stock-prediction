3e1c01bd9e7965abc2ef90b7a80a313c61cdd57d
Louan Flammanc <kusami.louan@gmail.com>
2024-10-19 20:41:29 -0400

    new api [unfinished]

--- stacking/main.py
+++ stacking/main.py
@@ -1,12 +1,27 @@
 from StackingModel import StackingModel
 import concurrent.futures
 import time
+import requests
+import pandas as pd
+import datetime
+
+def get_data():
+    url = "https://api.finazon.io/latest/finazon/us_stocks_essential/time_series"
+    
+    querystring = {"ticker":"AAPL","interval":"5m","page":"5","page_size":"1000","adjust":"all"}
+    
+    headers = {"Authorization": "apikey"}
+    
+    response = requests.get(url, headers=headers, params=querystring)
+    
+    print(len(response.json()))
+    print(type(response))
 
 def main():
-    start_time = time.time()
-    model = StackingModel("NVDX", "5m")
-
-    model.run()
+    get_data()
+    # start_time = time.time()
+    # model = StackingModel("NDX", "5m")
+    # model.run()
 
     # # multiprocessing (concurrent.futures)
     # with concurrent.futures.ProcessPoolExecutor() as executor:
@@ -25,8 +40,8 @@ def main():
     # for p in processes:
     #     p.join()
     
-    end_time = time.time()
-    print("Time taken: ", end_time-start_time)
+    # end_time = time.time()
+    # print("Time taken: ", end_time-start_time)
 
 if __name__ == "__main__":
     main()
