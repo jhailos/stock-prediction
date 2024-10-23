501e364bfdd90235c32aab67ed9603f767e0d479
Louan Flammanc <45247162+Gloobinours@users.noreply.github.com>
2024-10-23 12:01:08 -0400

    Added more info

--- stacking/main.py
+++ stacking/main.py
@@ -14,9 +14,23 @@ import os
 
 def main():
     start_time = time.time()
-    context = ContextData(ticker="AAPL", strategy=StrategyFinazon(), days=6, interval="1m")
+    context = ContextData(ticker="^GSPC", strategy=StrategyYfinance(), days=59, interval="2m")
     model = StackingModel(context.data, context.interval)
-    model.run()
+    rmse, rrmse, r2, predicted_price, last_price = model.run()
+
+    print('---------------------------')
+    lower_bound = predicted_price - rmse
+    upper_bound = predicted_price + rmse
+    print("> Upper bound: ", upper_bound)
+    print("> Actual: ", predicted_price)
+    print("> Lower bound: ", lower_bound)
+    if predicted_price < lower_bound:
+        print(f'# Going down {last_price - predicted_price}')
+    elif predicted_price > upper_bound:
+        print(f'# Going up by {predicted_price - last_price}')
+    else:
+        print('# RMSE too high to make accurate prediction')
+
 
     # # multiprocessing (concurrent.futures)
     # with concurrent.futures.ProcessPoolExecutor() as executor:
