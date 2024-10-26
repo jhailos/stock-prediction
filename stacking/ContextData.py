cfb206107c11f70c24a73be8c217152f81d0fed2
Louan Flammanc <kusami.louan@gmail.com>
2024-10-26 12:22:11 -0400

    added RSI

--- stacking/ContextData.py
+++ stacking/ContextData.py
@@ -3,9 +3,9 @@ from StrategyData import StrategyData
 import datetime
 import pandas as pd
 import os
+import math
 
 class ContextData:
-
     def __init__(self, ticker, strategy: StrategyData, days=None, interval=None, market_hours_only=True):
         self.strategy = strategy
         self.ticker = ticker
@@ -18,7 +18,7 @@ class ContextData:
         print(self.data)
 
     def read_csv(self):
-        return pd.read_csv(f'stock_data\{self.ticker}.csv', index_col='Datetime', parse_dates=True)
+        return pd.read_csv(f'stock_data\\{self.ticker}.csv', index_col='Datetime', parse_dates=True)
 
     def write_csv(self):
         outdir = "stock_data"
@@ -29,17 +29,21 @@ class ContextData:
 
     def compute_features(self):
         # EMA
-        # daily_closes = self.data['Close'].resample('1D').last()
-        # print(daily_closes)
-        # daily_closes = daily_closes.ffill()
-        self.data['EMA12'] = self.data['Close'].ewm(span=12*6.5*60/2).mean() # 12 days * 6.5 hours per day * min per hour
-        self.data['EMA26'] = self.data['Close'].ewm(span=26*6.5*60/2).mean()
-        # print(self.data['EMA12'])
+        self.data['EMA12'] = self.data['Close'].ewm(span=math.ceil(12*6.5*60/2)).mean() # 12 days * 6.5 hours per day * min per hour
+        self.data['EMA26'] = self.data['Close'].ewm(span=math.ceil(26*6.5*60/2)).mean()
+
         # MACD
         self.data['MACD'] = self.data['EMA12'] + self.data['EMA26']
 
         # MACD signal
         self.data['MACD_signal'] = self.data['MACD'].ewm(span=9).mean()
+
+        #RSI
+        delta = self.data['Close'].diff()
+        gain = (delta.where(delta > 0, 0)).rolling(window=math.ceil(14*6.5*60/2)).mean() # 14 days * 6.5 hours per day * min per hour
+        loss = (-delta.where(delta < 0, 0)).rolling(window=math.ceil(14*6.5*60/2)).mean()
+        relative_strength = gain / loss
+        self.data['RSI'] = 100 - (100 / (1 + relative_strength))
 
         # Price change
         self.data['price_change'] = self.data['Close'].pct_change()
