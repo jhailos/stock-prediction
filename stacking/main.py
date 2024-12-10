25ac870a29517f82ff98f4f131f6fd86f6ab1137
Louan Flammanc <45247162+Gloobinours@users.noreply.github.com>
2024-12-09 22:17:03 -0500

    graph showing normal distribution

--- stacking/main.py
+++ stacking/main.py
@@ -4,13 +4,19 @@ from StrategyYfinance import StrategyYfinance
 from StrategyFinazon import StrategyFinazon
 from StrategyAlphaVantage import StrategyAlphaVantage
 from StrategyAlpaca import StrategyAlpaca
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 import time
 
 def main():
     print('>Fetching data')
-    context = ContextData(ticker="SPY", strategy=StrategyAlpaca(), days=60, interval="1m", market_hours_only=False)
+    context = ContextData(ticker="SPY", strategy=StrategyAlpaca(), days=20, interval="1m", market_hours_only=False)
     start_time = time.time()
+    
+    sns.displot(data=context.data, x="price_change", kde=True)
+    plt.show()
+
     model = StackingModel(context.data, context.interval)
     rmse, rrmse, r2, predicted_price, last_price = model.run()
 
@@ -27,7 +33,7 @@ def main():
         print(f'# Going down by {last_price - predicted_price}')
     else:
         print('# RMSE too high to make accurate prediction')
-    
+
     end_time = time.time()
     print('---------------------------')
     print("Time taken: ", end_time-start_time)
